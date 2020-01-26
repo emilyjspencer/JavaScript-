f4f8858a260b367e7b8cd0de0670bd2e4e636dd6
lajoiedevivre89 <emilyspencer89@hotmail.co.uk>
2020-01-26 16:05:59 +0000

    Private count

--- closure3.js
+++ closure3.js
@@ -22,12 +22,20 @@ counter2()
 counter2()
 // 3
 
-//counter()
+// create new variable
 
+var counter3 = counter()
+// undefined
+
+counter3
 /* ƒ inner(){
     count++
     return count;
-  }
+  } */
 
-*/
+counter3()
+1
+
+// The counter3 variable doesn't modify the counter2 variable because 
+// both of the functions have their own private count variable 
 
