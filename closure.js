5532ce6c4b9346bb1fcc7a6083149025b8e2f91d
lajoiedevivre89 <emilyspencer89@hotmail.co.uk>
2020-01-26 13:05:22 +0000

    Closures

--- closure.js
+++ closure.js
@@ -1,9 +1,20 @@
 function outer() {
 var a = "I love"
-  return function inner(){
-    return a + " " + "cats"
+  return function inner(){ // inner() is a closure - because it makes use of variables defined in other functions
+    return a + " " + "cats" // that have previously returned 
     }
 }
 
-outer()
+outer() // [ƒ inner(){ return a + " " + "cats"}      just gives us the method definiton - no output
 outer()() // "I love cats"
+
+function a() {
+  var b = "I love Makers"
+    return function c() {
+        return b + " Obvs it's the best bootcamp"
+    }
+}
+
+a() //   method definition             f c() {  return b + "Obvs it's the best bootcamp" }
+a()()
+"I love Makers Obvs it's the best bootcamp"
