f7c105acdee5d97e63504acbf786aa789185fc07
lajoiedevivre89 <emilyspencer89@hotmail.co.uk>
2020-02-04 12:38:24 +0000

    For loops

---
+++ for_loops.js
@@ -0,0 +1,24 @@
+var text = ""
+for (i = 0; i < 11; i++) {
+  text += "Good Vibes Only " + i + "\n"
+}
+
+//Good Vibes Only 0Good Vibes Only 1
+//Good Vibes Only 2
+//Good Vibes Only 3
+//Good Vibes Only 4
+//Good Vibes Only 5Good Vibes Only 6
+//Good Vibes Only 7
+//Good Vibes Only 8
+//Good Vibes Only 9
+//Good Vibes Only 10
+
+
+
+var result = 1;
+
+for(var counter = 0; counter < 10; counter = counter + 1)
+result = result * 2
+console.log(result);
+
+// 1024
