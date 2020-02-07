0c979edf36883a8601c9c8debe2933295385e637
lajoiedevivre89 <emilyspencer89@hotmail.co.uk>
2020-02-07 22:56:45 +0000

    Lookup

---
+++ lookupLudovico.js
@@ -0,0 +1,13 @@
+var ludovicoEinaudi = {
+  1: "Primavera",
+  2: "Fly",
+  3: "Andare", 
+  4: "Una mattina", 
+  5: "Le Onde",
+  6: "I Giorni"
+}
+
+
+var song = 5;
+ludovicoEinaudi[song];
+// Le Onde
